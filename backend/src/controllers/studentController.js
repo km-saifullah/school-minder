ce6b18884c1f811801b8b0318db42d2f94e9d5ee
km-saifullah <kmsaifullah16@gmail.com>
2024-06-28 21:58:25 +0600

    get all students controller done

--- backend/src/controllers/studentController.js
+++ backend/src/controllers/studentController.js
@@ -3,10 +3,16 @@ import infoChecking from '../utils/infoChecking.js'
 
 // @desc:  show all students
 // @route: GET /api/v1/students
-const getAllStudents = (req, res) => {
+const getAllStudents = async (req, res) => {
+  let students = await Student.find()
   return res
     .status(200)
-    .json({ status: 'success', message: 'All students', data: '' })
+    .json({
+      status: 'success',
+      results: students.length,
+      message: 'All students',
+      data: students,
+    })
 }
 
 // @desc:  get a single student
@@ -16,8 +22,7 @@ const getSingleStudent = (req, res) => {}
 // @desc:  create a student
 // @route: POST /api/v1/students
 const createStudent = async (req, res) => {
-  const { fullName, email, password, confirmPassword, gender } =
-    req.body
+  const { fullName, email, password, confirmPassword, gender } = req.body
 
   // required field checking
   infoChecking(fullName, email, password, confirmPassword, res)
