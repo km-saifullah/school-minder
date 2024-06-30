9f1c6715e3701bd3c332a9c1b738220495da7eb2
km-saifullah <kmsaifullah16@gmail.com>
2024-06-30 20:52:46 +0600

    user some important middlewares for our application

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import { rateLimit } from 'express-rate-limit'
+import morgan from 'morgan'
 import studentRouter from './routes/studentRoutes.js'
 
 const app = express()
 
+// request limiter
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  limit: 20,
+  message: 'You have exceded maximum request per minute',
+})
+
+app.use(morgan('dev'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(limiter)
 
 // routes
 app.use('/api/v1/students', studentRouter)
--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,7 @@ import app from './app.js'
 import { hostname, port } from './config/index.js'
 import connectDb from './db/db.config.js'
 
-
-// connect database
-connectDb()
-
-// start the server
-app.listen(port, hostname, () =>
+app.listen(port, hostname, () => {
   console.log(`Server is running on port:${port}`)
-)
+  connectDb()
+})
